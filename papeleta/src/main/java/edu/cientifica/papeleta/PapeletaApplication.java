1dd4f865a5c0f33b39ef9ab587258272b1e9587e
Computer <Computer@192.168.1.7>
2020-05-14 11:17:28 -0500

    primer commit

---
+++ papeleta/src/main/java/edu/cientifica/papeleta/PapeletaApplication.java
@@ -0,0 +1,13 @@
+package edu.cientifica.papeleta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapeletaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapeletaApplication.class, args);
+	}
+
+}
